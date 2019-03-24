b4a25a3051395e3efbc7f64e703927fa4b3a9798
MohAzril <azril@alphatech.id>
2019-03-24 08:40:18 +0700

    add CORS

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -9,11 +9,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_jwt_extended import JWTManager
-
+from flask_cors import CORS
 
 #request.status.code
 app = Flask(__name__)
-
+CORS(app)
 # @app.route('/')
 # def index():
 #     return render_template('index.html')
